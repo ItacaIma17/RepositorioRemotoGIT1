ba337c3c21d3672407791e769a162010b8c2597b
imaalc17 <imanollapizondocarreras@gmail.com>
2025-03-17 20:13:34 +0100

    ok

--- Ejemplo1/src/Rama1/rama1.java
+++ Ejemplo1/src/Rama1/rama1.java
@@ -1,5 +1,9 @@
 package Rama1;
 
 public class rama1 {
-
+	public static void main (String [] args) {
+		System.out.print("La rama 1 ha sido fusionada");
+		System.out.println("Prueba 2 ");
+	
+	}
 }
--- Ejemplo1/src/rama2/rama2.java
+++ Ejemplo1/src/rama2/rama2.java
@@ -4,7 +4,7 @@ public class rama2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("La rama 2 ha sido creada");
 	}
 
 }
