b0dcf5b5fa215a4478a066e77c42f1fb8345cc40
imaalc17 <imanollapizondocarreras@gmail.com>
2025-03-17 11:52:46 +0100

    Modificaciones

---
+++ Ejemplo1/src/paquete1/principal1.java
@@ -0,0 +1,11 @@
+package paquete1;
+
+public class principal1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Primer commit en Git");
+
+	}
+
+}
