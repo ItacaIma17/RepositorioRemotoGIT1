4cbc5cdd49300088e05afc882d7b3318ff92ce50
imaalc17 <imanollapizondocarreras@gmail.com>
2025-03-17 17:02:12 +0100

    Cambios 3

--- Ejemplo1/src/paquete1/principal1.java
+++ Ejemplo1/src/paquete1/principal1.java
@@ -5,6 +5,10 @@ public class principal1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Primer commit en Git");
+		
+		System.out.println("El segundo comentario");
+		
+		System.out.print("Prueba 2 de cambios del archivo");
 
 	}
 
